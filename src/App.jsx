45c1b9328cc21ba744ba18d60643c6989663ad13
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-10-18 16:04:39 -0300

    vercel correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import FeedPost from '../src/screens/FeedPost'
+import FeedPost from './screens/FeedPost'
 import Cadastro from './screens/Cadastro'
 import Home from './screens/Home'
 import Feed from './screens/Feed'
@@ -14,7 +14,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/cadastro" element={<Cadastro/>} />
-        <Route path="/procurando_carona" element={<FeedPost/>} />
         <Route path="/post" element={<FeedPost/>} />
         <Route path="/home" element={<Home/>}/>
         <Route path="/feed" element={<Feed/>}/>
