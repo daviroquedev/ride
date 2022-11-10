750f7fff1b39be944b206172bad2add76e737be3
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-11-10 18:24:50 -0300

    imagens

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -26,6 +26,8 @@ export default function Profile() {
     const { user: userAuth } = useSelector((state) => state.auth)
     const { photos, loading: loadingPhoto, message: messagePhoto, error: errorPhoto } = useSelector((state) => state.photo)
 
+    console.log("isso é photos"+photos)
+
     const [title, setTitle] = useState("")
     const [image, setImage] = useState("")
 
@@ -109,32 +111,8 @@ export default function Profile() {
             )}
             <div className="user-photos">
                 <h2>Post publicados:</h2>
-                <div className="photos-container">
-                    {photos &&
-                        photos.map((photo) => (
-                            <div className="photo" key={photo._id}>
-                                {photo.image && (
-                                    <img
-                                        src={`${uploads}/photos/${photo.image}`}
-                                        alt={photo.title}
-                                    />
-                                )}
-                                {id === userAuth._id ? (
-                                    <div className="actions">
-                                        <Link to={`/photos/${photo._id}`}>
-                                            <BsFillEyeFill />
-                                        </Link>
-                                        <BsPencilFill onClick={() => handleEdit(photo)} />
-                                        <BsXLg onClick={() => handleDelete(photo._id)} />
-                                    </div>
-                                ) : (
-                                    <Link className="btn" to={`/photos/${photo._id}`}>
-                                        Ver
-                                    </Link>
-                                )}
-                            </div>
-                        ))}
-                    {photos.length === 0 && <p>Ainda não há fotos publicadas...</p>}
+                <div className="photo-container">
+                    <h2>ibagewns</h2>
                 </div>
             </div>
         </div>
