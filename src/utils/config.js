ced9d0dee6bdf5f9014239bff7f7528689148ac5
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-11-16 17:29:40 -0300

    method requestConfig

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,18 +7,18 @@ export const requestConfig = (method, data, token = null, image = null) => {
 
     if(image){
         config = {
-            method,
+            method: method,
             body: data,
-            headers: {}
-        }
+            headers: {},
+        };
     }else if(method === "DELETE" || data === null){
         config = {
-            method,
-            headers:{}
-        }
+            method: method,
+            headers:{},
+        };
     } else {
         config = {
-            method,
+            method: method,
             body: JSON.stringify(data),
             headers:{
                 "Content-Type":"aplication/json"
